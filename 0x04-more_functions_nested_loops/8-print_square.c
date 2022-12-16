8aa7430187ae1454e75972c41d002e66eb41c730
omolo-eng <billyomolo76@gmail.com>
2022-12-16 01:05:49 -0800

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,21 +6,22 @@
  */
 void print_square(int size)
 {
-		int a = 0;
-		int b = 0;
+	 if (size <= 0)
+	{
+		_putchar('\n');
+	} 
+	 else
+	{
+		int i, j;
 
-		if (size > 0)
-		{
-			while (a < size)
-			{
-				while (b < size)
-				{																			_putchar('#');
-					b++;	
-			b = 0;
-			a++;
-																					_putchar('\n');
-																					}
-		}
-		else
-			_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+		_putchar('#');
+	}
+		_putchar('\n');
+	}
+	}
 }
+
