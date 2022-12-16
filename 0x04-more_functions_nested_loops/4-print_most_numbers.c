b9860a2767f7936d43c033479fdcb9b675c35bc2
omolo-eng <billyomolo76@gmail.com>
2022-12-15 23:20:04 -0800

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - checks for checks for a digit (0 through 9).
+ * Return: Always 0.
+ */
+oid print_most_numbers(void)
+{
+		char c = '0';
+
+		while (c <= '9')
+		{
+			if (c != '2' && c != '4')
+			{
+				_putchar(c);
+			}
+		c++;
+		}
+_putchar('\n');
+}
