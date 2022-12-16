d1fbb6308cc1df82fdcac7d74ca4718f12567a92
omolo-eng <billyomolo76@gmail.com>
2022-12-15 23:50:49 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,19 +6,17 @@
  */
 void more_numbers(void)
 {
+ int i, j;
 
-		int a = 0;
-		int b = 0;
+  for (i = 0; i < 10; i++)
+	   {
+		    for (j = 0; j < 15; j++)
+			     {
+				      if (j >= 10)
+					       _putchar(j / 10 + '0');
+				       _putchar(j % 10 + '0');
+				        }
+		     _putchar('\n');
+		      }
 
-		while (b <= 9)
-		{
-			while (a <= 14)
-			{
-				if (a > 9)
-					_putchar(a / 10 + '0');															  _putchar(a % 10 + '0');															    a++;
-			}
-		_putchar('\n');
-		b++;
-		a = 0;
-		}
 }
