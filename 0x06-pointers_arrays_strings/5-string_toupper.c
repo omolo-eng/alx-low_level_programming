9b56954f5c567fdba7c9f085113e6e86edcdb82b
omolo-eng <billyomolo76@gmail.com>
2022-12-22 01:58:23 -0800

    string_touper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,22 @@
-#include "holberton.h"
-/**
- *  string_toupper - change lowercase letters to uppercase.
- *  @s: analized string.
- *  
- *  Return: String with all letters Uppercased.
- */
+#include "main.h"                                                                                                                    
+                                                                                                                                     
+/**                                                                                                                                  
+ *  string_toupper - Changes all lowercase letters                                                                                    
+ *                   of a string to uppercase.                                                                                        
+ *  @str: The string to be changed.                                                                                                   
+ *                                                                                                                                    
+ *  Return: A pointer to the changed string.                                                                                          
+*/                                                                                                                                  
 char *string_toupper(char *str)                                                                                                      
 {                                                                                                                                    
-	   int index = 0;                                                                                                               
+	int index = 0;                                                                                                               
 		                                                                                                                                     
 	while (str[index])                                                                                                           
 	{                                                                                                                            
-		if (str[index] >= 'a' && str[index] <= 'z')                                                                          
-			str[index] -= 32;                                                                                            
+	if (str[index] >= 'a' && str[index] <= 'z')                                                                          
+		str[index] -= 32;                                                                                            
 								                                                                                                                                     
-			index++;                                                                                             
+		index++;                                                                                                             
 	}                                                                                                                            
 			                                                                                                                                   return (str);                                                                                                                
 }
